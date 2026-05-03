470cd89ad4b2c0207fbfebf2755b67ca4f9c977b
YuliaSabitova <yuliasab2017@mail.com>
2026-05-03 20:36:05 +0300

    adding and deleting implemented

---
+++ src/Components/TodoItem/TodoItem.tsx
@@ -0,0 +1,43 @@
+import { Todo } from '../../Types/Todo';
+import '../../styles/todo.scss';
+import { Loader } from '../Loader/Loader';
+
+type Props = {
+  todo: Todo;
+  onDelete:(todoId: number) => void;
+  isLoading?: boolean;
+};
+
+export const TodoItem: React.FC<Props> = ({ todo, onDelete, isLoading = false }) => {
+  return (
+    <div data-cy="Todo" className={`todo ${todo.completed ? 'completed' : ''}`}>
+      <label className="todo__status-label" htmlFor={`todo-status-${todo.id}`}>
+        <input
+          id={`todo-status-${todo.id}`}
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={todo.completed}
+          readOnly
+          aria-label={`Статус справи: ${todo.title}`}
+         />
+      </label>
+
+      <span data-cy="TodoTitle" className="todo__title">
+        {todo.title}
+      </span>
+
+      <button
+      type="button"
+      className="todo__remove"
+      data-cy="TodoDelete"
+      onClick={() => onDelete(todo.id)}
+      disabled={isLoading}
+      >
+        ×
+      </button>
+
+      <Loader isLoading={isLoading} />
+    </div>
+  );
+};
